f315ca11567bac151b67718c7be6c049a7938a49
zuev720 <zuev720mail.ru>
2021-07-26 14:03:22 +0300

    Поправил css согластно макету

--- src/components/Portfolio/ProjectList/ProjectList.jsx
+++ src/components/Portfolio/ProjectList/ProjectList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+const shortid = require('shortid');
 
 export function ProjectList(props) {
     const filter = props.filter;
     const myWorks = (filter === 'All') ? props.myWorks : props.myWorks.filter((elem) => elem.category === filter);
 
     return (<div className={'ProjectList'}>{
-        myWorks.map((elem, index) => <img key={index} className={'work-image'} src={elem.img} alt={elem.category}/>)
+        myWorks.map((elem, index) => <img key={shortid.generate()} className={'work-image'} src={elem.img} alt={elem.category}/>)
     }</div>);
 }
 
